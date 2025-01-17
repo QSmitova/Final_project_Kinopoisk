98c465453d1e5ec408a2199d357a811fb0e23a74
QSmitova <qsmitova@gmail.com>
2025-01-17 21:24:15 +0400

    Delete page directory

--- page/extended_search_page.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-from page.base_page import BasePage
-
-
-class ExtendedSearchPage(BasePage):
-    def open_top_250_page(self):
-        self._driver.find_element(By.CSS_SELECTOR, "div.block_left_pad table ul>li:nth-child(1)").click()
